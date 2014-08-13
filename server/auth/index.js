d75aebcabc5d96ed9d23ecacc7314e1bbfc9eb04
Daniel Ku <kjunine@gmail.com>
2014-08-13 19:51:58 +0900

    Feature: 인증
    - 인증 관련 컴포넌트 추가
    - 유저 API 설정
    - 하지만 유저 Controller는 점검 필요(다음 마일스톤)
    
    close #5

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,20 +1,18 @@
 'use strict';
 
 var express = require('express');
-var config = loquire.config();
-var User = loquire.user('model');
 
 // Passport Configuration
-require('./local/passport').setup(User, config);
-require('./facebook/passport').setup(User, config);
-require('./google/passport').setup(User, config);
-require('./twitter/passport').setup(User, config);
+require('./local/passport').setup();
+// require('./facebook/passport').setup(User, config);
+// require('./google/passport').setup(User, config);
+// require('./twitter/passport').setup(User, config);
 
 var router = express.Router();
 
 router.use('/local', require('./local'));
-router.use('/facebook', require('./facebook'));
-router.use('/twitter', require('./twitter'));
-router.use('/google', require('./google'));
+// router.use('/facebook', require('./facebook'));
+// router.use('/twitter', require('./twitter'));
+// router.use('/google', require('./google'));
 
 module.exports = router;
